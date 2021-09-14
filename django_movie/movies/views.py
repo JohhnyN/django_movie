d1d48537a7811a9600f7c63db8a908d34f3524d7
JohnnyN <kazpiter@gmail.com>
2021-09-14 09:54:13 +0600

    2

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.http import request
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from .models import Movie
+from .models import Movie, Category
+from .forms import ReviewForm
 
 
 # Create your views here.
@@ -11,10 +13,24 @@ class MoviesView(ListView):
     """Список фильмов"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
-    #template_name = "movies/movie_list.html"
 
 
 class MovieDetailView(DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = "url"
+
+
+class AddReview(View):
+    """Отзывы"""
+
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            if request.POST.get("parent", None):
+                form.parent_id = int(request.POST.get("parent"))
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
